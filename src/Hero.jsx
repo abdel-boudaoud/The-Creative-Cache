6bc4d369aef15bde58ffadf9ae494af6fefb71b4
abdel-boudaoud <abdelkaderfarm@gmail.com>
2024-04-08 19:05:12 +0100

    path test

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,14 +5,14 @@ const Hero = () => {
         <div className="wrapper">
           <div className="text">
             <h1>Web design examples to inspire</h1>
-            <img id="mobile" src="/public/assets/hero-img.png" alt="" />
+            <img id="mobile" src="/assets/hero-img.png" alt="" />
             <p>
               <span>Handpicked </span>real-world examples. <br /> I don’t want
               your email, I just hope you enjoy :)
             </p>
           </div>
 
-          <img id="desktop" src="/public/assets/hero-img.png" alt="" />
+          <img id="desktop" src="/assets/hero-img.png" alt="" />
         </div>
       </div>
     </div>
